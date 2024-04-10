4454049a83b06ee87507a998e553366843c1cf05
Damon <damon@damongolding.com>
2024-04-10 17:03:38 +0100

    Init

---
+++ go.mod
@@ -0,0 +1,27 @@
+module clipserver
+
+go 1.22.2
+
+require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.10.0 // indirect
+	github.com/charmbracelet/log v0.4.0 // indirect
+	github.com/gen2brain/beeep v0.0.0-20240112042604-c7bb2cd88fea // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	golang.design/x/clipboard v0.7.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/exp/shiny v0.0.0-20240409090435-93d18d7e34b8 // indirect
+	golang.org/x/image v0.14.0 // indirect
+	golang.org/x/mobile v0.0.0-20231127183840-76ac6878050a // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/charmbracelet/log"
+	"github.com/gen2brain/beeep"
+	"golang.design/x/clipboard"
+	"net"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type JsonResponse struct {
+	Timestamp string `json:"timestamp"`
+	Code      int    `json:"code"`
+	Success   bool   `json:"success"`
+	Message   string `json:"message"`
+}
+
+func init() {
+	err := clipboard.Init()
+	if err != nil {
+		log.Fatalf(err.Error())
+		panic(err)
+	}
+}
+
+func GetLocalIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddress := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddress.IP
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprint(w, "ClipServer is running")
+		if err != nil {
+			return
+		}
+	})
+
+	mux.HandleFunc("POST /add", func(w http.ResponseWriter, r *http.Request) {
+
+		err := r.ParseForm()
+		if err != nil {
+			return
+		}
+
+		postData := r.Form.Get("clip")
+		postData = strings.TrimSpace(postData)
+
+		if postData == "" {
+			log.Error("missing clip form data")
+			return
+		}
+
+		data, err := base64.StdEncoding.DecodeString(postData)
+		if err != nil {
+			log.Error(err)
+			return
+		}
+		dataString := string(data)
+
+		log.Info("received post request", "message", dataString)
+
+		w.Header().Add("content-type", "application/json")
+
+		outJson := JsonResponse{
+			Timestamp: time.Now().String(),
+			Code:      200,
+			Success:   true,
+			Message:   dataString,
+		}
+
+		out, err := json.Marshal(outJson)
+		if err != nil {
+			return
+		}
+
+		clipboard.Write(clipboard.FmtText, data)
+
+		confirm := clipboard.Read(clipboard.FmtText)
+		if string(confirm) != dataString {
+			log.Error("missmatch", "confirm", string(confirm), "data", dataString)
+			return
+		}
+
+		_, err = w.Write(out)
+		if err != nil {
+			log.Error(err.Error())
+			return
+		}
+
+		err = beeep.Notify("Added to clipboard", dataString, "/Users/damon/GIT/clipserver/assets/iconfile.png")
+		if err != nil {
+			log.Error(err.Error())
+			return
+		}
+	})
+
+	fmt.Printf("Listening on %s:%s", GetLocalIP(), "8090\n")
+
+	err := http.ListenAndServe(":8090", mux)
+	if err != nil {
+		log.Fatalf(err.Error())
+		return
+	}
+}
